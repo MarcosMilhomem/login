1baef9202643f1d48054e2eec493eeee31be7942
Marcos Milhomem <marcosapolo511@gmail.com>
2023-12-04 13:54:32 -0300

    login

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-//rotação
-const rotation = document.querySelector('#lata-preta');
-const angulo = 45;
+const mode = document.getElementById('mode-icon');
+const look = document.querySelector ('#look')
 
-rotation.style.transform = `    rotate(${angulo}deg)`;
+mode.addEventListener ('click', () => {
+    const form = document.querySelector('#login-form')
+
+    if (mode.classList.contains('fa-moon')) {
+        mode.classList.remove ('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark');
+        return 
+    }
+        
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+    form.classList.remove('dark');
+    
+});
+
+look.addEventListener ('click', () => {
+    if (look.classList.contains('fa-eye')) {
+        look.classList.remove('fa-eye')
+        look.classList.add('fa-eye-slash')
+
+        return;
+    }
+    look.classList.add('fa-eye');
+    look.classList.remove('fa-eye-slash');
+});
